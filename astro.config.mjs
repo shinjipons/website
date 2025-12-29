668e469f1cd5e2aa319a4d050e9d8006e5db2e7e
Shinji Pons <github@shinjipons.com>
2025-12-29 22:11:47 +0100

    opengraph image fix attempt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.PUBLIC_SITE_URL || "https://shinjipons.com", // Update with your actual production URL
   integrations: [],
 });
