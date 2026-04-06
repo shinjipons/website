c4cae2d54787222a1b31497e0ce5ba5faa8edc7d
Shinji Pons <github@shinjipons.com>
2026-04-07 00:25:28 +0200

    cleaner home page without archive grid

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: process.env.PUBLIC_SITE_URL || "https://shinjipons.com", // Update with your actual production URL
   integrations: [],
+  markdown: {
+    allowHTML: true,
+  },
 });
